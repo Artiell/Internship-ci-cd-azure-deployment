255d113f66da92b19b3b1b55057627e3f7ded118
Artiell <61852335+Artiell@users.noreply.github.com>
2022-05-17 15:26:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -113,9 +113,5 @@ def sentiment():
 
     return render_template('sentiment.html')
 
-import os
-port = int(os.environ.get('PORT', 8080))
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run()
