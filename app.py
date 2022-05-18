64a47a571064494f382127716a2c1dd55a43bf5e
Runhilia <60939697+Runhilia@users.noreply.github.com>
2022-05-18 09:30:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -114,4 +114,5 @@ def sentiment():
     return render_template('sentiment.html')
 
 if __name__ == '__main__':
-    app.run()
+    port= int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
